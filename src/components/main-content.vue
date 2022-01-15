d6525e9ca44c1b34e3ea0e7da8bdec92b9dd918a
Firulais <sfabrizzio@hotmail.com>
2022-01-15 12:54:12 -0500

    first commit

---
+++ src/components/main-content.vue
@@ -0,0 +1,24 @@
+<template>
+
+<v-layout row wrap align-center>
+    <v-flex XS9 offset-md2>
+   <div v-for="article in articles" :key="article.title">
+  <v-card class="my-3" hover>
+<v-img height="350px"  max-width="1080" v-bind:src="article.urlToImage">
+
+</v-img>
+  </v-card>
+   </div>
+    </v-flex>
+</v-layout>
+</template>
+<script>
+
+
+export default ({
+   name:"main-content",
+   props : {
+       articles :Array
+   }
+})
+</script>
